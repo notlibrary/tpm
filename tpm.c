060446d30a3c9e794e09adb09fe3682008b691f2
yesfire <40695473+yesfire@users.noreply.github.com>
2026-03-01 09:26:31 +0300

    Header

--- tpm.c
+++ tpm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #include "prng64_xrp32.c"
 #include "prng64_xrp32.h"
@@ -15,7 +16,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
-#include <string.h>
 #endif
 
 
