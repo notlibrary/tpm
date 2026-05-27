de772daa052695f0e6dc88e8bdd16ab3eacd71c6
yesfire <40695473+yesfire@users.noreply.github.com>
2026-05-27 17:08:47 +0300

    Add 3 more tests

--- tests/tpm_battery.c
+++ tests/tpm_battery.c
@@ -1,8 +1,10 @@
 /*
-This is basically for unbelievers and skeptics we ran unit test battery consist of as
-much tests as possible against TPM API using oldest and most reliable C unit test framework libcheck
-I believe in dead simplicity theory underlying circular query algorithm is so dead simple that 
-it's just impossible to fail making most of tests useless anyway We'll see what really happens here
+	TPM Toothpaste Picking Manager source code 0BSD license
+	
+	This is basically for unbelievers and skeptics we ran unit test battery consist of as
+	much tests as possible against TPM API using oldest and most reliable C unit test framework libcheck
+	I believe in dead simplicity theory underlying circular query algorithm is so dead simple that 
+	it's just impossible to fail making most of tests useless anyway We'll see what really happens here
 */
 
 #include <check.h>
@@ -39,6 +41,52 @@ START_TEST (null_pick_CSV)
 }
 END_TEST
 
+START_TEST (length_pick_CSV)
+{
+	static list_node_t* toothpastes_list;
+	toothpaste_pick_t pick;
+	toothpaste_pick_options_t topts;
+	int len;
+	
+	toothpastes_list=tpm_load_list_from_file("not/existimg/path");
+	pick=tpm_pick_toothpaste(toothpastes_list,topts);
+	char* out=tpm_get_toothpaste_picking_CSV(pick);
+	len=strlen(out);
+	ck_assert_int_ge(0,len);
+}
+END_TEST
+
+START_TEST (length_pick_CSV)
+{
+	static list_node_t* toothpastes_list;
+	toothpaste_pick_t pick;
+	toothpaste_pick_options_t topts;
+	int len;
+	
+	toothpastes_list=tpm_load_list_from_file("not/existimg/path");
+	pick=tpm_pick_toothpaste(toothpastes_list,topts);
+	char* out=tpm_get_toothpaste_picking_JSON(pick);
+	len=strlen(out);
+	ck_assert_int_ge(0,len);
+}
+END_TEST
+
+START_TEST (length_pick_CSV)
+{
+	static list_node_t* toothpastes_list;
+	toothpaste_pick_t pick;
+	toothpaste_pick_options_t topts;
+	int len;
+	
+	toothpastes_list=tpm_load_list_from_file("not/existimg/path");
+	pick=tpm_pick_toothpaste(toothpastes_list,topts);
+	char* out=tpm_get_toothpaste_picking_message(pick);
+	len=strlen(out);
+	ck_assert_int_ge(0,len);
+}
+END_TEST
+
+
 Suite* tpm_suite(void)
 {
      Suite *s;
@@ -53,6 +101,9 @@ Suite* tpm_suite(void)
      tcase_add_test(tc_null_msg, null_pick_msg);
 	 tcase_add_test(tc_null_msg, null_pick_JSON);
 	 tcase_add_test(tc_null_msg, null_pick_CSV);
+     tcase_add_test(tc_null_msg, length_pick_msg);
+	 tcase_add_test(tc_null_msg, length_pick_JSON);
+	 tcase_add_test(tc_null_msg, length_pick_CSV);
 	 
      suite_add_tcase(s, tc_null_msg);
      
