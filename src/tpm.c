44453e245beb1c0e39483c904dec2665c413c4c2
yesfire <40695473+yesfire@users.noreply.github.com>
2026-06-30 17:56:46 +0300

    Small fixes

--- src/tpm.c
+++ src/tpm.c
@@ -98,7 +98,8 @@ static const char* user_strings[TOTAL_USER_MESSAGES]={
 	"Compiled on:",
 	"Anonymous",
 	"Output pick to file ",
-	"Compiler unknown"
+	"Brand None",
+	"Compiler unknown",
 	"Usage:",
 	"BUILTIN TOOTHPASTE 1",
 	"BUILTIN TOOTHPASTE 2",
